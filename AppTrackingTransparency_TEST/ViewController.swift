9776f6eaab5c54c7652f3eea69b85b7e580d7417
soduma <soduma@naver.com>
2021-05-05 17:41:59 +0900

    Initial Commit

---
+++ AppTrackingTransparency_TEST/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppTrackingTransparency_TEST
+//
+//  Created by 장기화 on 2021/05/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
